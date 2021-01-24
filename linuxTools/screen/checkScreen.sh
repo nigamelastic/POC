c8da5c1e70f83b6808244d63d55093127dda4035
Kshitij Nigam <nigamelastic@gmail.com>
2021-01-24 22:18:13 +0100

    Delete linuxTools directory

--- linuxTools/screen/checkScreen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Starting the script"
-screen -dmS jeher ./timer.sh
-
-#screen -dmS jeher 
-#screen -S jeher -X stuff './timer.sh^M'
-echo "finished"
-echo "use \"screen -r jeher\" --> to resume the screen"
